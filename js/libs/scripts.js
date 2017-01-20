35d86c4735705abdcfbd6be3b2714bff09bc4818
Tom Rhoads <terhoads1@gmail.com>
2017-01-19 20:25:49 -0800

    Update scripts.js

--- js/libs/scripts.js
+++ js/libs/scripts.js
@@ -116,7 +116,7 @@ var ParkLocation = function(value) {
 };
 
 //view model
-var viewModel = function() {
+var ViewModel = function() {
   var self = this;
 
   //Initial Map View variables
@@ -125,6 +125,7 @@ var viewModel = function() {
   var center = new google.maps.LatLng(latitude, longitude);
 
   //KO Observables
+  self.visibleList = ko.observable(true);
   self.initialParkLocations = ko.observableArray([]);
   self.parkLocations = ko.observableArray([]);
   self.query = ko.observable('');
@@ -218,7 +219,9 @@ var viewModel = function() {
     });
   };
 
-  
+  self.toggleClick = function() {
+    this.visibleList(!this.visibleList());
+  }
 
   //Click on item in list view to open infowindow and pan to location
   self.listViewClick = function(parkListItem) {
